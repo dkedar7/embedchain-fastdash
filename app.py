7120d900509df238ddf24a8ab5a9723b796cb1bb
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2023-08-21 16:05:50 -0400

    Handle Embedchain source reading errors

--- app.py
+++ app.py
@@ -18,27 +18,42 @@ Query: $query
 
 Answer:"""
 )
-query_config = QueryConfig(template=PROMPT, number_documents=5, max_tokens=2000, model="gpt-4")
+query_config = QueryConfig(
+    template=PROMPT, number_documents=5, max_tokens=2000, model="gpt-4"
+)
 
 # Define components
 openai_api_key_component = dmc.PasswordInput(
-        placeholder="API Key",
-        description="Get yours at https://platform.openai.com/account/api-keys",
-        required=True
-    )
+    placeholder="API Key",
+    description="Get yours at https://platform.openai.com/account/api-keys",
+    required=True,
+)
 
 web_page_urls_component = dmc.MultiSelect(
-        description="Include all the reference web URLs",
-        placeholder="Enter URLs separated by commas",
-        searchable=True,
-        creatable=True,
-    )
+    description="Include all the reference web URLs",
+    placeholder="Enter URLs separated by commas",
+    searchable=True,
+    creatable=True,
+)
 
-text_component = dmc.Textarea(placeholder="Write your query here", autosize=True, minRows=4, description="Any additional information that could be useful")
+text_component = dmc.Textarea(
+    placeholder="Write your query here",
+    autosize=True,
+    minRows=4,
+    description="Any additional information that could be useful",
+)
 
-query_component = dmc.Textarea(placeholder="Write your query here", autosize=True, minRows=4, required=True, description="Write your query here")
+query_component = dmc.Textarea(
+    placeholder="Write your query here",
+    autosize=True,
+    minRows=4,
+    required=True,
+    description="Write your query here",
+)
 
-answer_component = dcc.Markdown(style={"text-align": "left", "padding": "1%"}, link_target="_blank")
+answer_component = dcc.Markdown(
+    style={"text-align": "left", "padding": "1%"}, link_target="_blank"
+)
 
 
 def explore_your_knowledge_base(
@@ -54,17 +69,18 @@ def explore_your_knowledge_base(
     This app uses embedchain.ai, which abstracts the entire process of loading and chunking datasets, creating embeddings, and storing them in a vector database.
     Embedchain itself uses Langchain and OpenAI's ChatGPT API.
     """
+    answer_suffix = ""
+
+    if not openai_api_key:
+        return "Did you forget adding your OpenAI API key? If you don't have one, you can get it [here](https://platform.openai.com/account/api-keys)."
+
+    if not query:
+        return "Did you forget writing your query in the query box?"
+
     os.environ["OPENAI_API_KEY"] = openai_api_key
+    app = App()
 
     try:
-        app = App()
-
-        if not openai_api_key:
-            return "Did you forget adding your OpenAI API key? If you don't have one, you can get it [here](https://platform.openai.com/account/api-keys)."
-
-        if not query:
-            return "Did you forget writing your query in the query box?"
-
         if web_page_urls:
             [app.add("web_page", url) for url in web_page_urls]
 
@@ -77,15 +93,24 @@ def explore_your_knowledge_base(
         if text:
             app.add_local("text", text)
 
-        answer = app.query(query, query_config)
-
     except Exception as e:
-        answer = "Oops, something went wrong! Please try again later or make a suggestion [here](https://github.com/dkedar7/embedchain-fastdash/issues)."
+        print(str(e))
+        answer_suffix = "I couldn't analyze some sources. If you think this is an error, please try again later or make a suggestion [here](https://github.com/dkedar7/embedchain-fastdash/issues)."
+
+    answer = app.query(query, query_config)
+    answer = f"""{answer}
+
+    {answer_suffix}
+    """
 
     return answer
 
-# Build app (this is all it takes!). Fast Dash understands what it needs to do. 
-app = FastDash(explore_your_knowledge_base, github_url="https://github.com/dkedar7/embedchain-fastdash")
+
+# Build app (this is all it takes!). Fast Dash understands what it needs to do.
+app = FastDash(
+    explore_your_knowledge_base,
+    github_url="https://github.com/dkedar7/embedchain-fastdash",
+)
 server = app.server
 
 if __name__ == "__main__":
